6f79d86d2c606c45bbd8f0e36e988a1baae63a65
jaysi1701 <jaysigaur9449@gmail.com>
2026-06-11 15:39:53 +0530

    UC3 Add login form and validation

--- js/app.js
+++ js/app.js
@@ -1,5 +1,31 @@
-const startBtn = document.getElementById("startBtn");
+const loginForm =
+document.getElementById("loginForm");
 
-startBtn.addEventListener("click", () => {
-    alert("Welcome to Task Expense Manager!");
+const message =
+document.getElementById("message");
+
+function validateLogin(email, password){
+
+    if(email === "" || password === ""){
+        return "All fields are required";
+    }
+
+    return "Login Successful";
+}
+
+loginForm.addEventListener("submit", (event)=>{
+
+    event.preventDefault();
+
+    const email =
+    document.getElementById("email").value;
+
+    const password =
+    document.getElementById("password").value;
+
+    const result =
+    validateLogin(email, password);
+
+    message.textContent = result;
+
 });
